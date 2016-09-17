ceee42b13a6ed75de8805d2041931c80597da300
Eric B <eric.bezzam@epfl.ch>
2016-09-17 06:26:00 +0200

    removing test code

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    print (messaging_event)
 
                     response = diagnose.getResponse(message_text)
                     send_message(sender_id, response)
