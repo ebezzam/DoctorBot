e72bfe2569b9cdc65326893235e69f4f7482e1c0
Eric B <eric.bezzam@epfl.ch>
2016-09-17 07:56:03 +0200

    Debugging fb bot.

--- app.py
+++ app.py
@@ -38,11 +38,12 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
 
-                    print message_text
+                    message_text = messaging_event["message"]["text"]  # the message's text
                     response = diagnose.getResponse(message_text)
                     send_message(sender_id, response)
+
+                    
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
