2e849ee05101761acc5e775560a795a25d2e8906
Eric B <eric.bezzam@epfl.ch>
2016-09-17 21:07:28 +0200

    Fixed error.

--- app.py
+++ app.py
@@ -149,6 +149,7 @@ def send_message(recipient_id, message_text):
             "text": message_text
         }
     })
+    print data
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
