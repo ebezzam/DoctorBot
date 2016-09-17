f0fb112278278b8d891dbb3bf5f6d4e6e676292f
Eric B <eric.bezzam@epfl.ch>
2016-09-17 07:18:42 +0200

    trying to see what happens if there is no text

--- app.py
+++ app.py
@@ -38,7 +38,9 @@ def webhook():
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    messaging_text = ""
+                    if messaging_event["message"]["text"]:
+                    	message_text = messaging_event["message"]["text"]  # the message's text
 
                     response = diagnose.getResponse(message_text)
                     send_message(sender_id, response)
